b7d929e510e79a8af884efb6f3801216afbc11b7
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-05-03 23:25:43 +0900

    Initial commit

---
+++ src/main/java/Doit224p.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Doit224p {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arraylist = new ArrayList<Integer>();
+
+        arraylist.add(1);
+        arraylist.add(2);
+        arraylist.add(3);
+        arraylist.add(4);
+        arraylist.add(5);
+
+        System.out.println("리스트 특정값 구하기 : " + arraylist.get(3));
+    }
+}
